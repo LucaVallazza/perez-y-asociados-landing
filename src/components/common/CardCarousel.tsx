af81d943607747a5b4fdbf13a5d9214d39293440
Luca Vallazza <Luvallazza@gmail.com>
2025-03-15 04:14:00 -0300

    fix: styles import

--- src/components/common/CardCarousel.tsx
+++ src/components/common/CardCarousel.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Slider from 'react-slick';
 import { Box } from '@mui/material';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 interface SimpleCarouselProps {
   children: React.ReactNode[];
